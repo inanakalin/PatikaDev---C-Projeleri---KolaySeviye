3be726943664903176859bc4f5c029f5aeb66bdb
USER <USER@INAN>
2023-03-24 13:55:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Fibonacci(); // *** Kolay Seviye Projeler - Ortalama Hesaplama  *** Kulanıcıdan alınan derinliğe göre fibonacci serisindeki
+                     // rakamların ortalamasını alıp ekrana yazdıran uygulamayı yazınız.
+       
+
+    }
+
+    public static void Fibonacci ()   // Ortalama Hesaplama
+    {
+        Console.Write("Fibonacci seriniz kaç rakamdan oluşsun?");
+        float derinlik = int.Parse(Console.ReadLine());
+
+        float[] fibo = new float[(int)derinlik];
+        fibo[0] = 0; fibo[1] = 1; fibo[2] = 1;   
+
+        for(int i=3; i<fibo.Length; i++)
+        {
+            fibo[i] = fibo[i - 2] + fibo[i - 1];
+        }
+
+        float fiboort = fibo.Sum()/fibo.Length;
+
+        Console.WriteLine(fiboort);
+        
+    }
+}
